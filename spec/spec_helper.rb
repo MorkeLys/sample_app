d61bd3c08fd055f5dcc6b9eca7feb5a8af3b18d6
Geirskogul <marie.efimova@gmail.com>
2012-03-12 02:18:24 +0400

    Done with static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
@@ -24,4 +27,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+end
+end
+Spork.each_run do
 end
